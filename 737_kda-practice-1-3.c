ce47554bdc83fde1a24b5c85603477babb4f6418
Kalmykov Danil <kalmykov.danil.99@mail.ru>
2020-02-28 21:35:01 -0800

    bug fix

---
+++ 737_kda-practice-1-3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+	int num_1,num_2,res;
+	scanf("%d", &num_1);
+	scanf("%d", &num_2);
+	res = pow (num_1, num_2);
+	printf("%d\n", res);
+return 0;
+}
